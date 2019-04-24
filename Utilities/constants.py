dac2e1e878b3a6fa8d3429cabc01de54d48733a9
Ram <ram.ananth14@gmail.com>
2019-04-24 11:19:15 +0530

    Deleting unnecessary data

---
+++ Utilities/constants.py
@@ -0,0 +1,20 @@
+# Throughout this file x denotes excitation and m denotes emission
+
+# Tissue
+
+# mu_x = 150
+# mu_m = 
+# k_x = 
+# k_m = 
+# gamma = 
+# rho_x =
+# rho_m =
+
+# Fluorophore
+# mu_f_x = 
+# mu_f_m = 
+# k_f_x = 
+# k_f_m = 
+# gamma_f = 
+# rho_f_x =
+# rho_f_m =
--- appendix/continuous_time_inference (Burgers)/Burgers.py
+++ appendix/continuous_time_inference (Burgers)/Burgers.py
@@ -149,6 +149,9 @@ if __name__ == "__main__":
     t = data['t'].flatten()[:,None]
     x = data['x'].flatten()[:,None]
     Exact = np.real(data['usol']).T
+
+    print("AAAAAAAAAAAAAAAAAAAAAAAA",Exact.shape)
+    print("XXX", x.shape)
     
     X, T = np.meshgrid(x,t)
     
@@ -166,99 +169,100 @@ if __name__ == "__main__":
     xx3 = np.hstack((X[:,-1:], T[:,-1:]))
     uu3 = Exact[:,-1:]
     
-    X_u_train = np.vstack([xx1, xx2, xx3])
-    X_f_train = lb + (ub-lb)*lhs(2, N_f)
-    X_f_train = np.vstack((X_f_train, X_u_train))
-    u_train = np.vstack([uu1, uu2, uu3])
+    print(uu1.shape, uu2.shape,uu3.shape)
+    # X_u_train = np.vstack([xx1, xx2, xx3])
+    # X_f_train = lb + (ub-lb)*lhs(2, N_f)
+    # X_f_train = np.vstack((X_f_train, X_u_train))
+    # u_train = np.vstack([uu1, uu2, uu3])
     
-    idx = np.random.choice(X_u_train.shape[0], N_u, replace=False)
-    X_u_train = X_u_train[idx, :]
-    u_train = u_train[idx,:]
+    # idx = np.random.choice(X_u_train.shape[0], N_u, replace=False)
+    # X_u_train = X_u_train[idx, :]
+    # u_train = u_train[idx,:]
         
-    model = PhysicsInformedNN(X_u_train, u_train, X_f_train, layers, lb, ub, nu)
+    # model = PhysicsInformedNN(X_u_train, u_train, X_f_train, layers, lb, ub, nu)
     
-    start_time = time.time()                
-    model.train()
-    elapsed = time.time() - start_time                
-    print('Training time: %.4f' % (elapsed))
+    # start_time = time.time()                
+    # model.train()
+    # elapsed = time.time() - start_time                
+    # print('Training time: %.4f' % (elapsed))
     
-    u_pred, f_pred = model.predict(X_star)
+    # u_pred, f_pred = model.predict(X_star)
             
-    error_u = np.linalg.norm(u_star-u_pred,2)/np.linalg.norm(u_star,2)
-    print('Error u: %e' % (error_u))                     
+    # error_u = np.linalg.norm(u_star-u_pred,2)/np.linalg.norm(u_star,2)
+    # print('Error u: %e' % (error_u))                     
 
     
-    U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
-    Error = np.abs(Exact - U_pred)
+    # U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
+    # Error = np.abs(Exact - U_pred)
     
     
-    ######################################################################
-    ############################# Plotting ###############################
-    ######################################################################    
+    # ######################################################################
+    # ############################# Plotting ###############################
+    # ######################################################################    
     
-    fig, ax = newfig(1.0, 1.1)
-    ax.axis('off')
+    # fig, ax = newfig(1.0, 1.1)
+    # ax.axis('off')
     
-    ####### Row 0: u(t,x) ##################    
-    gs0 = gridspec.GridSpec(1, 2)
-    gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
-    ax = plt.subplot(gs0[:, :])
+    # ####### Row 0: u(t,x) ##################    
+    # gs0 = gridspec.GridSpec(1, 2)
+    # gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
+    # ax = plt.subplot(gs0[:, :])
     
-    h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
-                  extent=[t.min(), t.max(), x.min(), x.max()], 
-                  origin='lower', aspect='auto')
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.colorbar(h, cax=cax)
+    # h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
+    #               extent=[t.min(), t.max(), x.min(), x.max()], 
+    #               origin='lower', aspect='auto')
+    # divider = make_axes_locatable(ax)
+    # cax = divider.append_axes("right", size="5%", pad=0.05)
+    # fig.colorbar(h, cax=cax)
     
-    ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (u_train.shape[0]), markersize = 4, clip_on = False)
+    # ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (u_train.shape[0]), markersize = 4, clip_on = False)
     
-    line = np.linspace(x.min(), x.max(), 2)[:,None]
-    ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
-    ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
-    ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)    
+    # line = np.linspace(x.min(), x.max(), 2)[:,None]
+    # ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
+    # ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
+    # ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)    
     
-    ax.set_xlabel('$t$')
-    ax.set_ylabel('$x$')
-    ax.legend(frameon=False, loc = 'best')
-    ax.set_title('$u(t,x)$', fontsize = 10)
+    # ax.set_xlabel('$t$')
+    # ax.set_ylabel('$x$')
+    # ax.legend(frameon=False, loc = 'best')
+    # ax.set_title('$u(t,x)$', fontsize = 10)
     
-    ####### Row 1: u(t,x) slices ##################    
-    gs1 = gridspec.GridSpec(1, 3)
-    gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
+    # ####### Row 1: u(t,x) slices ##################    
+    # gs1 = gridspec.GridSpec(1, 3)
+    # gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
     
-    ax = plt.subplot(gs1[0, 0])
-    ax.plot(x,Exact[25,:], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$u(t,x)$')    
-    ax.set_title('$t = 0.25$', fontsize = 10)
-    ax.axis('square')
-    ax.set_xlim([-1.1,1.1])
-    ax.set_ylim([-1.1,1.1])
+    # ax = plt.subplot(gs1[0, 0])
+    # ax.plot(x,Exact[25,:], 'b-', linewidth = 2, label = 'Exact')       
+    # ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
+    # ax.set_xlabel('$x$')
+    # ax.set_ylabel('$u(t,x)$')    
+    # ax.set_title('$t = 0.25$', fontsize = 10)
+    # ax.axis('square')
+    # ax.set_xlim([-1.1,1.1])
+    # ax.set_ylim([-1.1,1.1])
     
-    ax = plt.subplot(gs1[0, 1])
-    ax.plot(x,Exact[50,:], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$u(t,x)$')
-    ax.axis('square')
-    ax.set_xlim([-1.1,1.1])
-    ax.set_ylim([-1.1,1.1])
-    ax.set_title('$t = 0.50$', fontsize = 10)
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
+    # ax = plt.subplot(gs1[0, 1])
+    # ax.plot(x,Exact[50,:], 'b-', linewidth = 2, label = 'Exact')       
+    # ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
+    # ax.set_xlabel('$x$')
+    # ax.set_ylabel('$u(t,x)$')
+    # ax.axis('square')
+    # ax.set_xlim([-1.1,1.1])
+    # ax.set_ylim([-1.1,1.1])
+    # ax.set_title('$t = 0.50$', fontsize = 10)
+    # ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
     
-    ax = plt.subplot(gs1[0, 2])
-    ax.plot(x,Exact[75,:], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$u(t,x)$')
-    ax.axis('square')
-    ax.set_xlim([-1.1,1.1])
-    ax.set_ylim([-1.1,1.1])    
-    ax.set_title('$t = 0.75$', fontsize = 10)
+    # ax = plt.subplot(gs1[0, 2])
+    # ax.plot(x,Exact[75,:], 'b-', linewidth = 2, label = 'Exact')       
+    # ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
+    # ax.set_xlabel('$x$')
+    # ax.set_ylabel('$u(t,x)$')
+    # ax.axis('square')
+    # ax.set_xlim([-1.1,1.1])
+    # ax.set_ylim([-1.1,1.1])    
+    # ax.set_title('$t = 0.75$', fontsize = 10)
     
-    # savefig('./figures/Burgers')  
+    # # savefig('./figures/Burgers')  
     
 
 
