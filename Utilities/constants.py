7aa122e464c453fcde29ea5dd3240deb4c655d22
rajamohanharesh <you@example.com>
2019-04-26 02:02:22 +0530

    test

--- Utilities/constants.py
+++ Utilities/constants.py
@@ -7,8 +7,8 @@ mu_m =  1.2
 k_x =4.902e-03 
 k_m =9.766e-03 
 gamma = 1 
-# rho_x =
-# rho_m =
+rho_x =0.2
+rho_m = 0.3
 
 # Fluorophore
 mu_f_x =1.1
@@ -16,7 +16,7 @@ mu_f_m = 0.3
 k_f_x = 4.9456e-03
 k_f_m = 9.94e-03
 gamma_f = 0.1 
-# rho_f_x =
-# rho_f_m =
+rho_f_x = 0.2
+rho_f_m = 0.3
 
-#q=
+q= 20
